c8f0e44c2e8652968fd55ec5fd5b97c405c419c3
biggicode <larioncescu@yahoo.com>
2022-03-20 09:42:29 +0200

    add semantic ui

--- src/components/app/app.component.jsx
+++ src/components/app/app.component.jsx
@@ -2,7 +2,7 @@ import React from "react"
 
 class App extends React.Component {
   render() {
-    return <div>This is app</div>
+    return <div className="ui container">This is app</div>
   }
 }
 
