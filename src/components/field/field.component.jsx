10d1722e51f459d625d3472ab4256d91570e61fe
biggicode <larioncescu@yahoo.com>
2022-03-20 09:58:30 +0200

    create field

---
+++ src/components/field/field.component.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+class Field extends React.Component {
+  render() {
+    ;<div className="ui field">
+      <label>Name</label>
+      <input type="text" />
+    </div>
+  }
+}
+
+export default Field
